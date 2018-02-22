a14c86e7c2879bdb91e9e41e8202a1dba9e6eddd
Pedro Santos <pedrosantosobral@gmail.com>
2018-02-22 21:59:21 +0000

    new commit

---
+++ cilindro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace cilindro
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string alt;
+            string rai;
+            double altc;
+            double raic;
+            double Volume;
+            double Superficie;
+
+
+            Console.WriteLine("Insira a altura do cilindro");
+            alt = Console.ReadLine();
+            altc = Convert.ToDouble(alt);
+            Console.WriteLine("Insira o raio do cilindro");
+            rai = Console.ReadLine();
+            raic = Convert.ToDouble(rai);
+
+            Volume = Math.PI * raic * raic * altc;
+            Console.WriteLine(Volume);
+
+            Superficie = 2 * Math.PI * raic * (raic + altc);
+            Console.WriteLine(Superficie);
+        }
+    }
+}
